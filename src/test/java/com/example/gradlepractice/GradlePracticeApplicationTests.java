6aaaf00567cfd981b5b8d9db50759dbe9b1cebe0
haseesah <hase1108shoiti@yahoo.co.jp>
2022-10-09 20:33:58 +0900

    springプロジェクト追加

---
+++ src/test/java/com/example/gradlepractice/GradlePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
